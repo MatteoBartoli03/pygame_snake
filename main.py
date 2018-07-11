803ed3c80c576caf954a26003093a47c3196c4c2
MatteoBartoli <matteobartoli01@gmail.com>
2018-07-11 15:30:39 +0200

    'work'

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ while True:
 	import pygame, sys, random, time
 	from class_snake import Snake
 	from class_food import foodSpawner
+	from pygame.locals import *
+	pygame.init()
 	while True:
-		window = pygame.display.set_mode((500, 500))
+		screen = pygame.display.set_mode((500, 500))
 		bg = pygame.image.load('prato_bg.jpg')
 		mela = pygame.image.load('mela.jpg')
 		pygame.display.set_icon(mela)
-		if life == 0:
-			bg = pygame.image.load('prato_2_bg.jpg')
 
-		pygame.display.set_caption("Snake game by Francesco & Matteo")
+		#pygame.display.set_caption("Snake game by Francesco & Matteo")
 		fps = pygame.time.Clock()
 
 		score = 0
@@ -43,25 +43,47 @@ while True:
 				score += 1
 				foodSpawner.setFoodOnScreen(False)
 
-			window.blit(bg, [0, 0])
+			screen.blit(bg, [0, 0])
 			for pos in snake.getBody():
-				pygame.draw.rect(window, pygame.Color(225,218,228), pygame.Rect(pos[0], pos[1], 10, 10))
-			pygame.draw.rect(window, pygame.Color(231,0,0), pygame.Rect(foodPos[0], foodPos[1], 10, 10))
+				pygame.draw.rect(screen, pygame.Color(225,218,228), pygame.Rect(pos[0], pos[1], 10, 10))
+			pygame.draw.rect(screen, pygame.Color(231,0,0), pygame.Rect(foodPos[0], foodPos[1], 10, 10))
 
 			if snake.checkCollision() == 1:
 				life -= 1
 				break
 
 			if life == 0:
-				bg = pygame.image.load('prato_2_bg.jpg')
+				bg = pygame.image.load('prato_bg.jpg')
+				font = pygame.font.SysFont("Comfortaa", 100)
+				surf_text = font.render("GAME OVER...", True, (0, 0, 0))
+				screen.blit(surf_text, (15, 200))                            
+				pygame.display.flip()
+				done = False
+				while not done:
+					for ev in pygame.event.get():
+							if ev.type == QUIT:
+								done = True
+					pygame.quit()
 			if life == -1:
 				gameOver()
 
 			LIFE_SCORE = "LIFE: " + str(life) + "   SCORE: " + str(score)
 			pygame.display.set_caption(LIFE_SCORE)
 			pygame.display.flip()
-			fps.tick(20)
+			fps.tick(7.5)
 
+			if score == 20:
+				bg = pygame.image.load('prato_bg.jpg')
+				font = pygame.font.SysFont("Comfortaa", 100)
+				surf_text = font.render("LEVEL 2", True, (0, 0, 0))
+				screen.blit(surf_text, (15, 200))                            
+				pygame.display.flip()
+				done = False
+				while not done:
+					for ev in pygame.event.get():
+							if ev.type == QUIT:
+								done = True
+					pygame.quit()
 
 		if snake.checkCollision() == 1:
 			break
