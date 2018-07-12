04ab749489274b2f07844a1282b856950617035c
MatteoBartoli <matteobartoli01@gmail.com>
2018-07-12 18:39:12 +0200

    'work'

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ life = 3
 speed = 10
 score = 0
 
+
 while True:
 	import pygame, sys, random, time
 	from class_snake import Snake
 	from class_food import foodSpawner
-	from class_wall import wall
 	from pygame.locals import *
 	from time import gmtime, strftime
 	pygame.init()
@@ -45,24 +45,40 @@ while True:
 				score += 1
 				foodSpawner.setFoodOnScreen(False)
 
+			if score == 5:
+				speed = 12.5
+			if score == 10:
+				speed = 15
+			if score == 2:
+				position_of_wall = [40, 100]
+				body_of_wall = []
+				for x in range(0, 30):
+					body_of_wall.append([(position_of_wall[0]+ (x*10)), 100])
+					body_of_wall.append([(position_of_wall[0]+ (x*10)), 110])
+
 			screen.blit(bg, [0, 0])
 
 			for pos in snake.getBody():
 				pygame.draw.rect(screen, pygame.Color(225,218,228), pygame.Rect(pos[0], pos[1], 10, 10))
 
-			#for pos in wall.Body():
-			#	pygame.draw.rect(screen, pygame.Color(225,218,228), pygame.Rect(pos[0], pos[1], 10, 10))
+			if score >= 2:
+				for pos in body_of_wall:
+					pygame.draw.rect(screen, pygame.Color(53,53,53), pygame.Rect(pos[0], pos[1], 10, 10))
 
 			pygame.draw.rect(screen, pygame.Color(231,0,0), pygame.Rect(foodPos[0], foodPos[1], 10, 10))
 
-			if snake.checkCollision() == 1:
+
+			a=0
+			if score >= 2:
+				for bodyPart in body_of_wall[1:]:
+					for partOfBody in snake.body[:]:
+						if partOfBody == bodyPart:
+							a = 1
+							break
+
+			if snake.checkCollision() == 1 or a == 1:
 				life -= 1
 				break
-
-			#if score >= 1:
-			#	if wall.check_collision() == 1:
-			#		life -= 1
-			# 		break
 
 			if life == 0:
 				bg = pygame.image.load('prato_bg.jpg')
@@ -78,46 +94,34 @@ while True:
 					pygame.quit()
 					sys.exit()
 
-			LIFE_SCORE = "LIFE: " + str(life) + "   SCORE: " + str(score)
-			bg = pygame.image.load('prato_bg.jpg')
-			font = pygame.font.SysFont("Comfortaa", 30)
-			surf_text = font.render(LIFE_SCORE, True, (255, 255, 255))
-			screen.blit(surf_text, (10, 10))                            
-			pygame.display.flip()
-			done = False
-			while True:
-				for ev in pygame.event.get():
-						if ev.type == QUIT:
-							break
-				break
+		#	LIFE_SCORE = "LIFE: " + str(life) + "   SCORE: " + str(score)
+		#	bg = pygame.image.load('prato_bg.jpg')
+		#	font = pygame.font.SysFont("Comfortaa", 30)
+		#	surf_text = font.render(LIFE_SCORE, True, (255, 255, 255))
+		#	screen.blit(surf_text, (10, 10))                            
+		#	pygame.display.flip()
+		#	done = False
+		#	while True:
+		#		for ev in pygame.event.get():
+		#				if ev.type == QUIT:
+		#					break
+		#		break
 
-			bg = pygame.image.load('prato_bg.jpg')
-			font = pygame.font.SysFont("Comfortaa", 20)
-			surf_text = font.render('SNAKE            FOOD', True, (255, 255, 255))
-			screen.blit(surf_text, (120, 555))                            
-			pygame.display.flip()
-			done = False
-			while True:
-				for ev in pygame.event.get():
-						if ev.type == QUIT:
-							break
-				break
-				
-
-
-
-			if score == 5:
-				speed = 12.5
-			elif score == 10:
-				speed = 15
-			elif score == 3:
-				pass			
-
-
+			#bg = pygame.image.load('prato_bg.jpg')
+			#font = pygame.font.SysFont("Comfortaa", 20)
+			#surf_text = font.render('SNAKE            FOOD            WALL', True, (255, 255, 255))
+			#screen.blit(surf_text, (120, 555))                            
+			#pygame.display.flip()
+			#done = False
+			#while True:
+			#	for ev in pygame.event.get():
+			#			if ev.type == QUIT:
+			#				break
+			#	break
 			
 			pygame.display.flip()
 			fps.tick(speed)
 
 
-		if snake.checkCollision() == 1:
+		if snake.checkCollision() == 1 or a == 1:
 			break
